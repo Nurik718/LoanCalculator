c555064831132b67bbbc7a685751089cafc3eb3f
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-02-07 11:29:42 +0600

    finished js

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const form = document.querySelector("#loan-form");
 
 form.addEventListener("submit", calculateResults);
 
+ const output=document.querySelector("#output");
+ output.style.display="none";
+const loader=document.querySelector("#loader");
+ loader.style.display="none";
+ loader.style.position="relative";
+
+ const img=document.querySelector("img");
+ img.style.textAlign="center";
+ img.style.width="160px";
+ img.style.height="200px";
+
+
+ const err=document.querySelector(".error");
+err.style.display="none";
+err.style.backgroundColor="blueviolet";
+err.style.height="40px";
+err.style.textAlign="center";
+err.style.fontSize="20px";
+
+
+
 function calculateResults(e) {
   const amount = document.querySelector("#amount");
   const interest = document.querySelector("#interest");
@@ -22,8 +43,18 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    loader.style.display="block";
+    err.style.display="none";
+    output.style.display="none";
+    function setTime(){
+      loader.style.display="none";
+      output.style.display="block";
+      err.style.display="none";
+    }
+    setTimeout(setTime,2500);
   } else {
-    alert("Заполните все поля!");
+   err.style.display="block";
+   output.style.display="none";
   }
 
   e.preventDefault();
